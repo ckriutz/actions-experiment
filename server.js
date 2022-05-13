522813ac30c4fc74f73baf2089c441fd19907889
ckriutz <casey.kriutzfield@live.com>
2022-05-13 15:10:20 -0400

    Added a route!

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function (req, res) {
+    res.send("<html><body><h1 style='text-align:center; padding-top:40px;'>Hello World</h1></body></html>");
+});
+
 // Allows you to set port in the project properties.
 app.set('port', process.env.PORT || 3000);
 
